71ef3a1f60c05ef636fff5e5a05b071a49d12839
Jack Brind <63796862+brindo91@users.noreply.github.com>
2025-03-22 19:08:33 +0000

    Revert URL changes - will fix in a branch

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/about.html'),
         kpis: resolve(__dirname, 'src/kpis.html'),
         knowledgebase: resolve(__dirname, 'src/knowledgebase.html'),
         complianceOverrides: resolve(
